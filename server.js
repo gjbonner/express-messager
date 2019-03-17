fa888b4bc830dcd4b55ba4eb5097e33d83e6ffdc
unknown <gjbonner@gmail.com>
2019-03-17 13:23:21 -0400

    username update display on disconnect

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ io.sockets.on('connection', function(socket){
     users.splice(users.indexOf(socket.username),1);
     updateUsernames();
     connections.splice(connections.indexOf(socket),1);
+    updateUsernames();
     console.log('Disconnected: %s sockets connected', connections.length);
   });
 
